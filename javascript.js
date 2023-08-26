6af3329c71a37b5d7898a81ef235fdf915232333
Anuj mishra <anuj14112000@gmail.com>
2023-08-26 18:10:10 +0530

    Complete DOM-JS

--- javascript.js
+++ javascript.js
@@ -1,6 +1,30 @@
+//  (function (){
+//     let newtext=document.querySelector(".changetext")
+//     console.log(newtext)
+//     newtext.innerText="Hello World"
+//     newtext.style.color="purple"
+//  })()
+  
+// const changeText = document.querySelector("#change-text"); 
+// changeText.addEventListener("click", function() {
+//      changeText.textContent = "Hello World"; 
+     
+//     });
+
 
 function replace(){
-    let newtext=document.getElementById("change").innerText;
-   newtext.innerText="hello world"
+    let newtext1=document.getElementById("change-text1")
+    newtext1.innerText="Welcome to Elevation Academy"
+    newtext1.style.color="RED"
 }
+
+  let h1text=document.getElementsByClassName("h1text")
+  h1text[0].setAttribute("style","color:red");
+  h1text[0].setAttribute("id","heading");
+
+  function changedirection(){
+  document.querySelector(".parent").style.flexDirection="column";
+  }
+
+
 
